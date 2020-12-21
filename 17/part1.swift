efd7fd05eb9630ff7e1e07f1e943da3471982cd6
Ryan Thomson <git@ryanthomson.net>
2020-12-21 13:16:11 -0500

    Add solution for day 17
    This might be my favorite day so far

---
+++ 17/part1.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+var activePositions = Set<Vec3>() // Stores all positions that are in an 'active' state
+
+// Load the initial state
+let input = try! String(contentsOfFile: "input.txt")
+input.split(separator: "\n").enumerated().forEach { (y, line) in
+	line.enumerated().forEach { (x, ch) in
+		if ch == "#" { activePositions.insert(Vec3(x: x, y: y, z: 0)) }
+	}
+}
+
+for _ in 1...6 {
+	// We need to run the simulation for every currently active position, plus everything adjacent
+	let simulationSpace = activePositions
+		.map { adjacentCoords(to: $0) }
+		.reduce(activePositions) { $0.union($1) }
+	
+	var activeNext = Set<Vec3>()
+	for pos in simulationSpace {
+		let activeAdj = adjacentCoords(to: pos).filter(activePositions.contains).count
+		if activePositions.contains(pos) && (activeAdj == 2 || activeAdj == 3) {
+			activeNext.insert(pos)
+		} else if !activePositions.contains(pos) && activeAdj == 3 {
+			activeNext.insert(pos)
+		}
+	}
+	
+	activePositions = activeNext
+}
+
+print(activePositions.count)
+
+// I could have sworn that Swift has hashable tuples...
+struct Vec3: Equatable, Hashable {
+	var x: Int
+	var y: Int
+	var z: Int
+}
+
+func adjacentCoords(to pos: Vec3) -> Set<Vec3> {
+	var adjacent: Set<Vec3> = []
+	for x in -1...1 {
+		for y in -1...1 {
+			for z in -1...1 {
+				guard x != 0 || y != 0 || z != 0 else { continue }
+				adjacent.insert(Vec3(x: pos.x + x, y: pos.y + y, z: pos.z + z))
+			}
+		}
+	}
+	return adjacent
+}
